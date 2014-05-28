76ac4db7f3e0c799d4108520ad852cb0c2a16ffd
NAULabs <kxaydin@gmail.com>
2014-05-28 08:55:22 -0500

    thanks!

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,26 @@
 from selenium import webdriver
+import unittest
+
+
 browser = webdriver.Firefox()
+
+# User checks the homepage
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+#She sees page title, which is To-Do Lists
+assert 'To-Do Lists' in browser.title, 'browser title was' + browser.title
+
+#She enters a new to-do item
+
+#She types "Buy peacock feathers"
+
+#When she hits enter, the page says:
+# 1: "Buy peacock feathers"
+
+#There is a textbox blank available for new entries all the time
+
+#She enters "Another one"
+
+#Her list is stored , when she comes back with a specific URL she can see her list
+
+browser.quit()
